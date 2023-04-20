7a2e5a5a89239334855e620483e9971cb9666234
Carloslpez219 <56690399+Carloslpez219@users.noreply.github.com>
2023-04-20 17:10:46 -0600

    modo oscuro y emviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,14 @@
 export const environment = {
   production: true,
-  // loginUrl: 'https://disatel.desarrollogt.net/LOGIN/API/API_login.php?request=',
   loginUrl: 'https://login2.disatel.app/LOGIN/API/API_login.php?request=',
+  // loginUrl: 'https://logindisatel.desarrollogt.net/LOGIN/API/API_login.php?request=',
+  // loginUrl: 'https://disatel.desarrollogt.net/LOGIN/API/API_login.php?request=',
   ajustesUrl: '/ROOT/API/API_ajustes.php?request=',
   disatelUrl: '/ROOT/API/API_ot_ver.php?request=',
   disatelEjecutar: '/ROOT/API/API_ot_ejecutar.php?request=',
   fotoVehiculo: '/ROOT/API/API_imagen_vehiculo.php?',
   fotoOrden: '/ROOT/API/API_imagen_ot.php?',
+  fotoDispositivo: '/APP/API_imagen_vehiculo.php?',
   notification: '/ROOT/API/API_pushup_notification.php?request=',
   coordenadas: '/ROOT/CPORDEN/FRMorden.php?',
   otEmergente: '/ROOT/API/API_ot_ejecutar_emergente.php?request='
